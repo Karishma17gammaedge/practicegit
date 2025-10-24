eba931a1482d0c8881a5bed731784751c0d23c2e
GunjanRathore0011 <your_personal_github_email@example.com>
2025-10-24 15:28:35 +0530

    made changes in home.js

--- home.js
+++ home.js
@@ -4,16 +4,22 @@ function loadHomePage() {
     // Simulate loading home page content
     console.log("Loading home page content...");
 }
+console.log("Changes by Gunjan...")
 
 loadHomePage();
+
+console.log(res);
 
 let a=10;
 let b=290;
 
 function add(a,b){
+    console.log(a+b)
     return a+b;
 }
 
+
+console.log(a+b)
 const res=add(a,b);
 
 console.log(res);
