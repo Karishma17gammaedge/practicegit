5c427e19c2afe2cad29493ddd9f9d038f2858e73
GunjanRathore0011 <your_personal_github_email@example.com>
2025-10-24 15:04:12 +0530

    adding home.js

---
+++ home.js
@@ -0,0 +1,19 @@
+console.log("Welcome to the Home Page!");
+
+function loadHomePage() {
+    // Simulate loading home page content
+    console.log("Loading home page content...");
+}
+
+loadHomePage();
+
+let a=10;
+let b=290;
+
+function add(a,b){
+    return a+b;
+}
+
+const res=add(a,b);
+
+console.log(res);
